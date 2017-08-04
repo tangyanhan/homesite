e8d31355b82a846f00802dc9c30ffb1e5089b3fc
Ethan Tang <ethan.tang@emc.com>
2017-08-04 14:51:54 +0800

    Added import page

--- manage_videos/views.py
+++ manage_videos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from django.http import Http404
+from django.http import HttpResponse
 from homesite.settings import config_dict
 from video.models import Video, KeywordCount
 
@@ -15,43 +15,56 @@ def index(request):
     return render(request, 'manage-videos.html')
 
 
+def import_videos(request):
+    return render(request, 'import-videos.html')
+
+
 def load_dir(request):
     dir = request.POST['dir']
 
     if len(dir) > 512:
-        raise Http404
+        return HttpResponse('Dir too long', status=400)
 
     upper_idx = dir.find('..')
 
     if upper_idx != -1:
         # Check if it contains '..' traps. For a sub path combined can be '/home/xxx/../../usr/local'
         if upper_idx != (len(dir) - 2):
-            raise Http404
+            return HttpResponse('Bad directory: {0}'.format(dir), status=400)
+
         match = re.match(r'(.*)/[^/]+/\.\.$', dir)
         dir = ""
         if match is not None:
             dir = match.groups()[0]
 
-    root = config_dict['load_dir_root']
+    root = config_dict.get('load_dir_root', os.getenv('HOME'))
     path = os.path.join(root, dir)
 
-    if not os.path.isdir(path):
-        raise Http404
+    if not path.startswith(root):
+        return HttpResponse('Requested dir not allowed', status=403)
 
+    if not os.path.isdir(path):
+        return HttpResponse('No such directory: {0}'.format(path), status=404)
+
+    dir_list = []
     file_list = []
+    if path != root:
+        dir_list.append(['..', True])
     for fn in os.listdir(path):
         if fn.startswith('.'):
             continue
 
         entry = os.path.join(path, fn)
 
-        is_dir = False
         if os.path.isdir(entry):
-            is_dir = True
-        file_list.append({'file': fn, 'isdir': is_dir})
+            dir_list.append([fn, True])
+        else:
+            file_list.append([fn, False])
+    entry_list = dir_list + file_list
 
+    headers = ['Path', 'Type']
     current_dir = path[len(root):]
-    return JsonResponse({'files': file_list, 'current_dir': current_dir})
+    return JsonResponse({'headers': headers, 'data': entry_list, 'current_dir': current_dir})
 
 
 def db(request):
@@ -77,6 +90,3 @@ def db(request):
 
     return JsonResponse({'headers': table_headers, 'data':data})
 
-
-def import_videos(request):
-    return render(request, 'import-videos.html')
