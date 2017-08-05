54b1810f69229b67d896b1bf4fca2caf847594ef
Ethan Tang <ethan.tang@emc.com>
2017-08-05 21:50:18 +0800

    More flexible and thin implement for reactive thumb layout

--- static/js/video-thumb.js
+++ static/js/video-thumb.js
@@ -1,38 +1,25 @@
 
 function createThumbFromVideo(video){
     li = document.createElement("li")
-    li.setAttribute("class", "thumb")
+    li.setAttribute("class", "thumb-container")
 
-    thumbContainer = document.createElement("div")
-    thumbContainer.setAttribute("class","thumb-container")
-        spanImg = document.createElement("span")
-            spanImg.setAttribute("class", "thumb-image")
-                a = document.createElement("a")
-                a.setAttribute("class","item-link")
-                a.href = "/play/" + video.id
-                    img = document.createElement("img")
-                    img.src = "/static/thumb/" + video.id + ".png"
-                    img.setAttribute("data-image-error-source","/static/images/no-img.png")
-                    img.setAttribute("flip-template", "/static/flip/" + video.id + "-{index}.png")
-                    img.className += " flip"
-                a.append(img)
-            spanImg.append(a)
-    thumbContainer.append(spanImg)
+    li.setAttribute("link", "/play/" + video.id)
+        img = document.createElement("img")
+        img.src = "/static/thumb/" + video.id + ".png"
+        img.setAttribute("data-image-error-source","/static/images/no-img.png")
+        img.setAttribute("flip-template", "/static/flip/" + video.id + "-{index}.png")
+        img.className += "thumb-image flip"
+    li.append(img)
         lenSpan = document.createElement("span")
         lenSpan.setAttribute("class","length")
         lenSpan.innerHTML = durationStringFromSeconds(video.duration)
-        thumbContainer.append(lenSpan)
+        li.append(lenSpan)
 
         titleSpan = document.createElement("span")
         titleSpan.setAttribute("class","title")
-            titleLink = document.createElement("a")
-            titleLink.setAttribute("class","title-item-link")
-            titleLink.href = "/play/" + video.id
-            titleLink.innerHTML = video.title
+            titleSpan.innerHTML = video.title
 
-            titleSpan.append(titleLink)
-        thumbContainer.append(titleSpan)
+        li.append(titleSpan)
 
-    li.append(thumbContainer)
     return li
 }
