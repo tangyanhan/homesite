cd73cccbec52d26414ecd0cb6ae4b191a61c2b51
Ethan Tang <yanhan.tang@hotmail.com>
2017-08-07 14:05:27 +0800

    Use './manage.py flush' instead

--- clean_db_sqlite.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM video_video;
-DELETE FROM sqlite_sequence WHERE name='video_video';
-DELETE FROM video_keywordcount;
-DELETE FROM sqlite_sequence WHERE name='video_keywordcount';
-DELETE FROM video_KeywordVideoId;
-DELETE FROM sqlite_sequence WHERE name='video_KeywordVideoId';
-VACUUM;
-
